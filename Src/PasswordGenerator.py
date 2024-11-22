44f4d64b96c46cc18142a9edb9049db623fa4df0
Lee Rogers <lrogers0941@gmail.com>
2024-11-21 19:22:37 -0800

    Updated interface to include password generator

--- Src/PasswordGenerator.py
+++ Src/PasswordGenerator.py
@@ -1,17 +1,7 @@
 import random
 import string
 
-
-def password_generator():
-    num_chars_option = input("Enter number of characters (choose from 8-24): ")
-    num_chars = int(num_chars_option)
-    cap_option = input("Include capital letters? (y/n) ").lower().strip() == 'y'
-    capitalize = bool(cap_option)
-    num_option = input("Include numbers? (y/n) ").lower().strip() == 'y'
-    include_numbers = bool(num_option)
-    include_specialchar_option = input ("Include special characters? (y/n) ").lower().strip() == 'y'
-    include_specialchar = bool(include_specialchar_option)
-
+def password_generator(num_chars, capitalize, include_numbers, include_specialchar):
     # include_words_option = input ("Include words? (y/n) ").lower().strip() == 'y'
     # include_words = bool(include_words_option)
 
@@ -28,4 +18,4 @@ def password_generator():
     for i in range(num_chars):
         password = password + random.choice(characters)
 
-    print(password)
+    return password
--- Src/UserInterface.py
+++ Src/UserInterface.py
@@ -1,4 +1,5 @@
 from PassphraseGenerator import passphrase_generator
+from PasswordGenerator import password_generator
 import sys
 
 # https://customtkinter.tomschimansky.com/
@@ -16,7 +17,7 @@ class PassphraseGeneratorFrame(ctk.CTkFrame):
       self.num_words_entry = ctk.CTkEntry(self, placeholder_text="Num. Words")
       self.num_words_entry.grid(row=1, column=0, padx=10, pady=10, sticky="we")
 
-      self.capitalize_check_box = ctk.CTkCheckBox(self, text="Capitlize")
+      self.capitalize_check_box = ctk.CTkCheckBox(self, text="Capitalize")
       self.capitalize_check_box.grid(row=2, column=0, padx=10, pady=10, sticky="w")
 
       self.include_numbers_check_box = ctk.CTkCheckBox(self, text="Incl. Numbers")
@@ -35,6 +36,38 @@ class PassphraseGeneratorFrame(ctk.CTkFrame):
    def generate(self):
       result = passphrase_generator(num_words=int(self.num_words_entry.get()), capitalize=self.capitalize_check_box.get()
                                     , include_numbers=self.include_numbers_check_box.get(), separator=self.separator_entry.get())
+      self.result_text_box.delete("0.0", str(sys.maxsize) + ".0")
+      self.result_text_box.insert("0.0", result)
+
+class PasswordGeneratorFrame(ctk.CTkFrame):
+   def __init__(self, master):
+      super().__init__(master)
+
+      self.label = ctk.CTkLabel(self, text="Password Generator")
+      self.label.grid(row=0, column=0, padx=10, pady=10, sticky="we")
+
+      self.num_chars_entry = ctk.CTkEntry(self, placeholder_text="Num. Characters (8-24)")
+      self.num_chars_entry.grid(row=1, column=0, padx=10, pady=10, sticky="we")
+
+      self.capitalize_check_box = ctk.CTkCheckBox(self, text="Incl. Capitals")
+      self.capitalize_check_box.grid(row=2, column=0, padx=10, pady=10, sticky="w")
+
+      self.include_numbers_check_box = ctk.CTkCheckBox(self, text="Incl. Numbers")
+      self.include_numbers_check_box.grid(row=3, column=0, padx=10, pady=10, sticky="w")
+
+      self.include_specialchar_check_box = ctk.CTkCheckBox(self, text="Incl. Special Characters")
+      self.include_specialchar_check_box.grid(row=4, column=0, padx=10, pady=10, sticky="we")
+
+      self.result_text_box = ctk.CTkTextbox(self, height=50)
+      self.result_text_box.grid(row=5, column=0, padx=10, pady=10)
+      self.result_text_box.insert("0.0", "Password3 Placeholder1")
+
+      self.generate_button = ctk.CTkButton(self, text="Generate", command=self.generate)
+      self.generate_button.grid(row=6, column=0, padx=10, pady=10, sticky="we")
+
+   def generate(self):
+      result = password_generator(num_chars=int(self.num_chars_entry.get()), capitalize=self.capitalize_check_box.get()
+                                    , include_numbers=self.include_numbers_check_box.get(), include_specialchar=self.include_specialchar_check_box.get())
       self.result_text_box.delete("0.0", str(sys.maxsize) + ".0")
       self.result_text_box.insert("0.0", result)
 
@@ -59,10 +92,13 @@ class App(ctk.CTk):
       super().__init__()
 
       self.title("Password Manager")
-      self.geometry("420x370")
+      self.geometry("660x370")
 
       self.passphrase_generator_frame = PassphraseGeneratorFrame(self)
       self.passphrase_generator_frame.grid(row=0, column=0, padx=10, pady=10, sticky="n")
 
+      self.password_generator_frame = PasswordGeneratorFrame(self)
+      self.password_generator_frame.grid(row=0, column=1, padx=10, pady=10, sticky="n")
+
       self.password_management_frame = PasswordManagementFrame(self)
-      self.password_management_frame.grid(row=0, column=1, padx=10, pady=10, sticky="n")
+      self.password_management_frame.grid(row=0, column=2, padx=10, pady=10, sticky="n")
