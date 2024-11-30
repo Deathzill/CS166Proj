24a1f5cc7c51f5f2221253f19d23eed98fd2a158
Dana Shakrovsky <dana.shakrovsky@sjsu.edu>
2024-11-29 22:11:31 -0800

    updated random password generator again

--- Src/PasswordGenerator.py
+++ Src/PasswordGenerator.py
@@ -1,19 +1,19 @@
 import random
+import re
 import string
 
 def password_generator(num_chars, capitalize, include_numbers, include_specialchar):
-    # include_words_option = input ("Include words? (y/n) ").lower().strip() == 'y'
-    # include_words = bool(include_words_option)
-
     password = ""
     characters = string.ascii_lowercase
+    special_char = string.punctuation
+    special_char += "!@#$%^&*(),.?:{}|<>"
 
     if capitalize:
         characters += string.ascii_uppercase
     if include_numbers:
         characters += string.digits
     if include_specialchar:
-        characters += string.punctuation
+        characters += special_char
 
     for i in range(num_chars):
         password = password + random.choice(characters)
@@ -22,14 +22,13 @@ def password_generator(num_chars, capitalize, include_numbers, include_specialch
     check_capital = False
     check_special_char = False
 
-    for char in password:
-        if include_numbers and char.isdigit():
-            check_num = True
-        if capitalize and char.isupper():
-            check_capital = True
+    if include_numbers:
+        check_num = re.search(r'[0-9]', password)
+    if capitalize:
+        check_capital = re.search(r'[A-Z]', password)
 
     if include_specialchar:
-        check_special_char = any(not char.isalnum() for char in password)
+        check_special_char = bool(re.search(r'[!@#$%^&*(),.?":{}|<>]', password))
 
     p = list(password)
     if include_numbers and not check_num:
@@ -37,8 +36,7 @@ def password_generator(num_chars, capitalize, include_numbers, include_specialch
     if include_numbers and not check_capital:
         p[len(password) - 2] = random.choice(string.ascii_uppercase)
     if include_specialchar and not check_special_char:
-        p[len(password) - 3] = random.choice(string.punctuation)
+        p[len(password) - 3] = random.choice(special_char)
 
     password = "".join(p)
-
     return password
